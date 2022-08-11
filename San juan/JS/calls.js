c037a3967c0e381909acdf9b29fb8464d7e192ce
Oscar Ortiz <104640780+Oscar-GOB98@users.noreply.github.com>
2022-08-10 19:11:15 -0600

    San Juan
    Kenny Roberto Ortiz Barillas

---
+++ San juan/JS/calls.js
@@ -0,0 +1,113 @@
+function ocultar(){
+    document.getElementById('introduccion').style.display = 'block';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+function ocultar1(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'block';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'flex';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+function ocultar2(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'block';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'flex';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+function ocultar3(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'block';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'flex';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+function ocultar4(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'block';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'flex';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+function ocultar5(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'block';
+    document.getElementById('caracteristicas').style.display = 'none';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'flex';
+}
+
+function ocultar6(){
+    document.getElementById('introduccion').style.display = 'none';
+    document.getElementById('educativas').style.display = 'none';
+    document.getElementById('sociales').style.display = 'none';
+    document.getElementById('comercio').style.display = 'none';
+    document.getElementById('nube').style.display = 'none';
+    document.getElementById('audiovisuales').style.display = 'none';
+    document.getElementById('caracteristicas').style.display = 'block';
+    document.getElementById('imagesaju').style.display = 'none';
+    document.getElementById('imagesaju1').style.display = 'none';
+    document.getElementById('imagesaju2').style.display = 'none';
+    document.getElementById('imagesaju3').style.display = 'none';
+    document.getElementById('imagesaju4').style.display = 'none';
+    document.getElementById('imagesaju5').style.display = 'none';
+}
+
+
